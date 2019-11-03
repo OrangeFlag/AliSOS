46a8be753ec40c9cfe4e86c2aa55dca10fb56be8
karasikrus <karasikrus@gmail.com>
2019-11-03 20:40:38 +0300

    empty doctor equals null

--- AliSOS Mock clinic server/routes/index.js
+++ AliSOS Mock clinic server/routes/index.js
@@ -23,28 +23,17 @@ router.post('/patient', function (req, res, next) {
         patient.doctorType = req.body.doctorType;
     }
     if (patient.userId && patient.phone && patient.anamnesis && patient.address) {
-        if (patient.doctorType) {
 
-            db.query(
-                "INSERT INTO public.patient (\"userid\", \"anamnesis\", \"address\", \"phone\", \"doctortype\", \"time\", \"id\") VALUES ($1, $2, $3, $4, $5, to_timestamp($6 / 1000.0), DEFAULT)",
-                [patient.userId, patient.anamnesis, patient.address, patient.phone, patient.doctorType, patient.timestamp])
-                .then(function () {
-                    res.send(JSON.stringify(patient));
-                })
-                .catch(function (error) {
-                    console.log("ERROR:", error);
-                });
-        } else {
-            db.query(
-                "INSERT INTO public.patient (\"userid\", \"anamnesis\", \"address\", \"phone\", \"time\", \"id\") VALUES ($1, $2, $3, $4, to_timestamp($5 / 1000.0), DEFAULT)",
-                [patient.userId, patient.anamnesis, patient.address, patient.phone, patient.timestamp])
-                .then(function () {
-                    res.send(JSON.stringify(patient));
-                })
-                .catch(function (error) {
-                    console.log("ERROR:", error);
-                });
-        }
+        db.query(
+            "INSERT INTO public.patient (\"userid\", \"anamnesis\", \"address\", \"phone\", \"doctortype\", \"time\", \"id\") VALUES ($1, $2, $3, $4, $5, to_timestamp($6 / 1000.0), DEFAULT)",
+            [patient.userId, patient.anamnesis, patient.address, patient.phone, patient.doctorType, patient.timestamp])
+            .then(function () {
+                res.send(JSON.stringify(patient));
+            })
+            .catch(function (error) {
+                console.log("ERROR:", error);
+            });
+
     } else {
         res.status(400).send('incorrect patient data');
     }
