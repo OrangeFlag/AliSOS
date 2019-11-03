405f1cf47d943b14bf3f0ea95fa39957e3f1eafd
karasikrus <karasikrus@gmail.com>
2019-11-03 20:49:35 +0300

    Mock clinique
    * initial
    
    * parse patient
    
    * check received patient data
    
    * index shows info
    
    * postgres works
    
    * timestamps; 400 error code
    
    * database url from configuration file
    
    * directory change(moved into subdirectory); gitignore change
    
    * small changes
    
    * empty doctor equals null

---
+++ AliSOS Mock clinic server/routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+
+var pgp = require("pg-promise")(/*options*/);
+var dbUrl = require('./../dbConfig')
+var db = pgp(dbUrl.postgresUrl);
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'AliSOS Mock clinic'});
+});
+
+router.post('/patient', function (req, res, next) {
+    const patient = {
+        "userId": req.body.userId,
+        "anamnesis": req.body.anamnesis,
+        "address": req.body.address,
+        "phone": req.body.phone,
+        "timestamp": Date.now()
+    }
+    if (req.body.doctorType) {
+        patient.doctorType = req.body.doctorType;
+    }
+    if (patient.userId && patient.phone && patient.anamnesis && patient.address) {
+
+        db.query(
+            "INSERT INTO public.patient (\"userid\", \"anamnesis\", \"address\", \"phone\", \"doctortype\", \"time\", \"id\") VALUES ($1, $2, $3, $4, $5, to_timestamp($6 / 1000.0), DEFAULT)",
+            [patient.userId, patient.anamnesis, patient.address, patient.phone, patient.doctorType, patient.timestamp])
+            .then(function () {
+                res.send(JSON.stringify(patient));
+            })
+            .catch(function (error) {
+                console.log("ERROR:", error);
+            });
+
+    } else {
+        res.status(400).send('incorrect patient data');
+    }
+
+
+});
+
+module.exports = router;
