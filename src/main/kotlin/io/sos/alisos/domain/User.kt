06bf248f72e85d5fd615e2055d947cf43880864b
Ilya S <AccountAll@yandex.ru>
2019-11-06 21:12:42 +0300

    feat: add clinic client call (#11)
    this resolves #6

--- src/main/kotlin/io/sos/alisos/domain/User.kt
+++ src/main/kotlin/io/sos/alisos/domain/User.kt
@@ -1,23 +1,15 @@
 package io.sos.alisos.domain
 
 data class User(
-    var anamnesis: String? = null,
-    var address: String? = null,
-    var phone: String? = null
+    val anamnesis: String? = null,
+    val address: String? = null,
+    val phone: String? = null
 ) {
-    fun fill(other: User) {
-        if (anamnesis == null) {
-            anamnesis = other.anamnesis
-        } else if (other.anamnesis != null) {
-            anamnesis += other.anamnesis
-        }
-
-        if (other.address != null) {
-            address = other.address
-        }
-
-        if (other.phone != null) {
-            phone = other.phone
-        }
+    fun fill(other: User): User {
+        return User(
+            anamnesis ?: other.anamnesis,
+            address ?: other.address,
+            phone ?: other.phone
+        )
     }
 }
