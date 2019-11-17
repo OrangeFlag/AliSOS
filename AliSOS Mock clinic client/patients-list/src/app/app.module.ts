8dd648500f4b839710f622e770be80c4ab477f87
karasikrus <karasikrus@gmail.com>
2019-11-17 17:36:15 +0300

    empty patients component created

--- AliSOS Mock clinic client/patients-list/src/app/app.module.ts
+++ AliSOS Mock clinic client/patients-list/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientsComponent } from './patients/patients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientsComponent
   ],
   imports: [
     BrowserModule,
