dce81895b3f4a5dd0809afc4e74cf3076a77af25
karasikrus <karasikrus@gmail.com>
2019-11-17 19:48:24 +0300

    works locally without styles

--- AliSOS Mock clinic client/patients-list/src/app/patients.service.ts
+++ AliSOS Mock clinic client/patients-list/src/app/patients.service.ts
@@ -2,17 +2,20 @@ import {Injectable} from '@angular/core';
 import {Patient} from "./patient";
 import {PATIENTS} from "./mock-patients";
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientsService {
+  private apiUrl = 'http://localhost:3000/api/patients/'
 
   getPatients(): Observable<Patient[]> {
-    return of(PATIENTS);
+    return this.http.get<Patient[]>(this.apiUrl);
   }
 
-  constructor() {
+  constructor( private http: HttpClient) {
   }
 }
